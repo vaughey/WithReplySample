922c6bf3ccdaeb6735c0c73c5a828f854e31b438
Parker E. Vaughey <parker@vaughey.com>
2024-07-24 12:52:02 -0400

    GIT

--- WithReplySample/ViewController.swift
+++ WithReplySample/ViewController.swift
@@ -22,16 +22,17 @@ struct ViewController: UIViewRepresentable {
             
             if message.name == parent.JavaScriptAPIObjectName, let messageBody = message.body as? String {
                 
-                
                 print("namespaceWithinTheInjectedJSCode WkScriptMessage body: \(messageBody)")
                 print("Calling replyHandler with 2.2 as the return value and nil as the err string.")
                 
                 // Mentally translate replyHandler: (Any?, String?) into replyHandler: (result: Any?, err: String?)
-                // If a string - anything other than nil - is provided in the second parameter (err) of replyHandler (result: Any?, err: String?), the errorFunc( err ) is run in javascript. err takes priority over result. The plumbing is hidden.
+                // If a string - anything other than nil - is provided in the second parameter (err) of replyHandler
+                // (result: Any?, err: String?), the errorFunc( err ) is run in javascript.
+                // err takes priority over result. The plumbing is hidden.
                 //If a value for  result: Any is provided with nil for err, successFunc( result ) is run in javascript.
                 
-                replyHandler(2.2,nil) // first var is success return val, second is err string if error
-                //replyHandler(nil,"2.2") // first var is success return val, second is err string if error
+                replyHandler(2.2,nil) // "err" is nil so use result and run success handler in javascript
+                //replyHandler(2.2,"anything") // err is NOT nil so run err handler in javascript and ignore result
                 
             }
         }
