2eefd9cc668ff6271ec097ab4ab15aac64cbf550
Parker E. Vaughey <parker@vaughey.com>
2024-07-24 10:13:55 -0400

    working with console logs

--- WithReplySample/WithReplySampleApp.swift
+++ WithReplySample/WithReplySampleApp.swift
@@ -3,7 +3,7 @@
 //  WithReplySample
 //
 //  Created by Parker E. Vaughey on 7/23/24.
-//
+//  BASED ON: https://stackoverflow.com/questions/65270083/how-to-use-wkscriptmessagehandlerwithreply-in-swiftui
 
 import SwiftUI
 
