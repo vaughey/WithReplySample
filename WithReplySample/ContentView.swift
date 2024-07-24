dd9614a25e19611d18fb8e3c3d62a306b25d2eef
Parker E. Vaughey <parker@vaughey.com>
2024-07-23 21:10:49 -0400

    Initial Commit

---
+++ WithReplySample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WithReplySample
+//
+//  Created by Parker E. Vaughey on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WithReplySample/WithReplySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithReplySampleApp.swift
+//  WithReplySample
+//
+//  Created by Parker E. Vaughey on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WithReplySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
